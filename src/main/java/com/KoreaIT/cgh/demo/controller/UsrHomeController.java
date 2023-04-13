6218c83aeac3837d4e5113002be3d98a28b7def0
guenhyuck <guenhyuck94@gmail.com>
2023-04-13 17:22:33 +0900

    ArticleController 추가, 게시글 작성 구현, 리스트 구현

--- src/main/java/com/KoreaIT/cgh/demo/controller/UsrHomeController.java
+++ src/main/java/com/KoreaIT/cgh/demo/controller/UsrHomeController.java
@@ -1,4 +1,4 @@
-package com.KoreaIT.cwy.demo.controller;
+package com.KoreaIT.cgh.demo.controller;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,26 +16,7 @@ import lombok.NoArgsConstructor;
 @Controller
 public class UsrHomeController {
 
-	@RequestMapping("/usr/home/getArticles")
-	@ResponseBody
-	public List<Article> getArticles() {
-		Article article1 = new Article(1, "제목1");
-		Article article2 = new Article(2, "제목2");
-
-		List<Article> articles = new ArrayList<>();
-		articles.add(article1);
-		articles.add(article2);
-
-		return articles;
-	}
-
-	@RequestMapping("/usr/home/getArticle")
-	@ResponseBody
-	public Article getArticle() {
-		Article article = new Article(1, "제목1");
-
-		return article;
-	}
+	
 
 	@RequestMapping("/usr/home/getList")
 	@ResponseBody
@@ -94,11 +75,3 @@ public class UsrHomeController {
 	}
 }
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-class Article {
-	private int id;
-	private String title;
-
-}
