db4f55b765926be976a67dc0da6f6d50b27c5cab
guenhyuck <guenhyuck94@gmail.com>
2023-04-14 17:07:26 +0900

    Insert 쿼리 실행 되도록 return 타입 수정, last_insert_id (수정)

--- src/main/java/com/KoreaIT/cgh/demo/controller/UsrArticleController.java
+++ src/main/java/com/KoreaIT/cgh/demo/controller/UsrArticleController.java
@@ -1,15 +1,18 @@
 package com.KoreaIT.cgh.demo.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.KoreaIT.cgh.demo.service.ArticleService;
 import com.KoreaIT.cgh.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
+
 	@Autowired
 	private ArticleService articleService;
 
@@ -21,7 +24,9 @@ public class UsrArticleController {
 		if (article == null) {
 			return id + "번 글은 존재하지 않습니다";
 		}
+
 		articleService.modifyArticle(id, title, body);
+
 		return article;
 	}
 
@@ -32,7 +37,9 @@ public class UsrArticleController {
 		if (article == null) {
 			return id + "번 글은 존재하지 않습니다";
 		}
+
 		articleService.deleteArticle(id);
+
 		return id + "번 글을 삭제했습니다";
 	}
 
@@ -61,6 +68,8 @@ public class UsrArticleController {
 		if (article == null) {
 			return id + "번 글은 존재하지 않습니다";
 		}
+
 		return article;
 	}
+
 }
