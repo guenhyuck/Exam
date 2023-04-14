b912ca86ea75e10a87d71fa268c20084329a6a1f
guenhyuck <guenhyuck94@gmail.com>
2023-04-14 13:48:58 +0900

    게시물 상세보기, Object

--- src/main/java/com/KoreaIT/cgh/demo/controller/UsrArticleController.java
+++ src/main/java/com/KoreaIT/cgh/demo/controller/UsrArticleController.java
@@ -2,11 +2,9 @@ package com.KoreaIT.cgh.demo.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.KoreaIT.cgh.demo.vo.Article;
 
 @Controller
@@ -18,7 +16,6 @@ public class UsrArticleController {
 	public UsrArticleController() {
 		lastArticleId = 0;
 		articles = new ArrayList<>();
-
 		makeTestData();
 	}
 
@@ -27,28 +24,13 @@ public class UsrArticleController {
 		for (int i = 1; i <= 10; i++) {
 			String title = "제목 " + i;
 			String body = "내용 " + i;
-
 			writeArticle(title, body);
 		}
 	}
 
-	private void modifyArticle(int id, String title, String body) {
-		Article article = getArticle(id);
-		
-		article.setTitle(title);
-		article.setBody(body);
-		
-	}
-	private void deleteArticle(int id) {
-		Article article = getArticle(id);
-		articles.remove(article);
-		
-	}
 	public Article writeArticle(String title, String body) {
 		int id = lastArticleId + 1;
-
 		Article article = new Article(id, title, body);
-
 		articles.add(article);
 		lastArticleId++;
 		return article;
@@ -58,13 +40,48 @@ public class UsrArticleController {
 		for (Article article : articles) {
 			if (article.getId() == id) {
 				return article;
-
 			}
 		}
 		return null;
 	}
 
-	// 액션 메서드
+	private void deleteArticle(int id) {
+		Article article = getArticle(id);
+		articles.remove(article);
+	}
+
+	private void modifyArticle(int id, String title, String body) {
+		Article article = getArticle(id);
+		article.setTitle(title);
+		article.setBody(body);
+	}
+
+	// 액션메서드
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public Object doModify(int id, String title, String body) {
+		Article article = getArticle(id);
+		if (article == null) {
+			return id + "번 글은 존재하지 않습니다";
+		}
+
+		modifyArticle(id, title, body);
+
+		return article;
+	}
+
+	@RequestMapping("/usr/article/doDelete")
+
+	@ResponseBody
+	public String doDelete(int id) {
+		Article article = getArticle(id);
+		if (article == null) {
+			return id + "번 글은 존재하지 않습니다";
+		}
+		deleteArticle(id);
+		return id + "번 글을 삭제했습니다";
+	}
+
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
@@ -75,37 +92,20 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
-
 		return articles;
 	}
 
-	@RequestMapping("/usr/article/doDelete")
+	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
-	private String doDelete(int id) {
+	public Object getArticleAction(int id) {
+
 		Article article = getArticle(id);
-		if(article == null) {
+
+		if (article == null) {
 			return id + "번 글은 존재하지 않습니다";
 		}
-		
-		deleteArticle(id);
-		
-		return id + "번글을 삭제했습니다";
 
-	}
-
-
-	@RequestMapping("/usr/article/doModify")
-	@ResponseBody
-	private String doModify(int id,String title,String body) {
-		Article article = getArticle(id);
-		if(article == null) {
-			return id + "번 글은 존재하지 않습니다";
-		}
-		
-		modifyArticle(id,title,body);
-		
-		return id + "번글을 수정했습니다";
-
+		return article;
 	}
 
 }
--- src/main/java/com/KoreaIT/cgh/demo/vo/Article.java
+++ src/main/java/com/KoreaIT/cgh/demo/vo/Article.java
@@ -12,4 +12,5 @@ public class Article {
 	private String title;
 	private String body;
 
+
 }
