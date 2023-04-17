39ea84e6fc5f9e3b6d8858b63643670898e3a63a
guenhyuck <guenhyuck94@gmail.com>
2023-04-17 14:19:28 +0900

    회원가입 기능 구현중

--- DB.sql
+++ DB.sql
@@ -12,6 +12,18 @@ CREATE TABLE article(
     `body` TEXT NOT NULL
 );
 
+# 회원 테이블 생성
+CREATE TABLE `member`(
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    loginId  CHAR(100) NOT NULL,
+    loginPw CHAR(100) NOT NULL,
+    `name` TEXT NOT NULL,
+    nickname TEXT NOT NULL,
+    cellphonenum INT(100) NOT NULL,
+    email TEXT NOT NULL
+);
+
 # 게시물 테스트데이터 생성
 INSERT INTO article 
 SET regDate = NOW(),
@@ -28,15 +40,46 @@ title = '제목 2',
 INSERT INTO article 
 SET regDate = NOW(),
 updateDate = NOW(),
-
 title = '제목 3',
-
 `body` = '내용 3';
+
+# 회원  테스트데이터 생성
+
+INSERT INTO `member` 
+SET regDate = NOW(),
+loginId = 'test1',
+loginPw = 'test1',
+`name` = 'test1',
+nickname = 'test1',
+cellphonenum = '1234',
+email = 'test1';
+
+
+INSERT INTO `member` 
+SET regDate = NOW(),
+loginId = 'test2',
+loginPw = 'test2',
+`name` = 'test2',
+nickname = 'test2',
+cellphonenum = '12345',
+email = 'test2';
+
+
+INSERT INTO `member` 
+SET regDate = NOW(),
+loginId = 'test3',
+loginPw = 'test3',
+`name` = 'test3',
+nickname = 'test3',
+cellphonenum = '123456',
+email = 'test3';
 
 
 
 
 SELECT * FROM article;
+
+SELECT * FROM `member`;
 
 
 
