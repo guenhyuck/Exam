25ebf2e53b703e95896aa31d438c178d88080ceb
guenhyuck <guenhyuck94@gmail.com>
2023-04-19 15:59:33 +0900

    JSP로 Article List 구현

--- src/main/java/com/KoreaIT/cgh/demo/controller/UsrHomeController.java
+++ src/main/java/com/KoreaIT/cgh/demo/controller/UsrHomeController.java
@@ -1,23 +1,19 @@
 package com.KoreaIT.cgh.demo.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.KoreaIT.cgh.demo.vo.Article;
 
 @Controller
 public class UsrHomeController {
 
 	@RequestMapping("/usr/home/main")
-	@ResponseBody
 	public String showMain() {
+		return "usr/home/main";
+	}
 
-		return "Main";
+	@RequestMapping("/")
+	public String showRoot() {
+		return "redirect:/usr/home/main";
 	}
 
 }
