52551918bdc592946b908177e61830c39d630cf1
guenhyuck <guenhyuck94@gmail.com>
2023-04-20 16:42:08 +0900

    게시물 삭제 시 뒤로가기 또는 지정된 경로로 이동

--- demo-1/src/main/java/com/KoreaIT/cgh/demo/controller/UsrArticleController.java
+++ demo-1/src/main/java/com/KoreaIT/cgh/demo/controller/UsrArticleController.java
@@ -14,7 +14,13 @@ import com.KoreaIT.cgh.demo.vo.ResultData;
 public class UsrArticleController {
 	@Autowired
 	private ArticleService articleService;
+	
+	
+	
+	
+	
 	// 액션메서드
+	//글수정
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
 	public ResultData<Integer> doModify(HttpSession httpSession, int id, String title, String body) {
@@ -37,28 +43,37 @@ public class UsrArticleController {
 		}
 		return articleService.modifyArticle(id, title, body);
 	}
+	
+	
+	
+	//글삭제
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
-	public ResultData<Integer> doDelete(HttpSession httpSession, int id) {
+	public String doDelete(HttpSession httpSession, int id) {
 		boolean isLogined = false;
 		int loginedMemberId = 0;
-		if (httpSession.getAttribute("loginedMemberId") != null) {
-			isLogined = true;
-			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
-		}
+
 		if (isLogined == false) {
-			return ResultData.from("F-A", "로그인 후 이용해주세요");
+			return Ut.jsHitoryBack("F-A", "로그인 후 이용해주세요");
 		}
+
 		Article article = articleService.getArticle(id);
 		if (article == null) {
-			return ResultData.from("F-1", Ut.f("%d번 글은 존재하지 않습니다", id), "id", id);
+			return Ut.jsHitoryBack("F-1", Ut.f("%d번 글은 존재하지 않습니다", id));
 		}
+
 		if (article.getMemberId() != loginedMemberId) {
-			return ResultData.from("F-2", Ut.f("%d번 글에 대한 권한이 없습니다", id));
+			return Ut.jsHitoryBack("F-2", Ut.f("%d번 글에 대한 권한이 없습니다", id));
 		}
+
 		articleService.deleteArticle(id);
-		return ResultData.from("S-1", Ut.f("%d번 글을 삭제 했습니다", id), "id", id);
+
+		return Ut.jsReplace(Ut.f("%d번 글을 삭제 했습니다", id), "../article/list");
 	}
+	
+	
+	
+	//글쓰기
 	@RequestMapping("/usr/article/doWrite")
 	@ResponseBody
 	public ResultData<Article> doWrite(HttpSession httpSession, String title, String body) {
@@ -82,7 +97,9 @@ public class UsrArticleController {
 		Article article = articleService.getArticle(id);
 		return ResultData.newData(writeArticleRd, "article", article);
 	}
-
+	
+	
+    //글 목록
 	@RequestMapping("/usr/article/list")
 	public String showList(Model model) {
 		List<Article> articles = articleService.getForPrintArticles();
@@ -91,7 +108,9 @@ public class UsrArticleController {
 
 		return "usr/article/list";
 	}
-
+	
+	
+    //글 상세보기
 	@RequestMapping("/usr/article/detail")
 	public String showDetail(HttpSession httpSession, Model model, int id) {
 		boolean isLogined = false;
--- demo-1/src/main/java/com/KoreaIT/cgh/demo/controller/UsrMemberController.java
+++ demo-1/src/main/java/com/KoreaIT/cgh/demo/controller/UsrMemberController.java
@@ -16,7 +16,43 @@ import com.KoreaIT.cgh.demo.vo.ResultData;
 public class UsrMemberController {
 	@Autowired
 	private MemberService memberService;
-
+	
+	
+	
+	//회원가입
+	@RequestMapping("/usr/member/doJoin")
+	@ResponseBody
+	public ResultData<Member> doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
+			String email) {
+		if (Ut.empty(loginId)) {
+			return ResultData.from("F-1", "아이디를 입력해주세요");
+		}
+		if (Ut.empty(loginPw)) {
+			return ResultData.from("F-2", "비밀번호를 입력해주세요");
+		}
+		if (Ut.empty(name)) {
+			return ResultData.from("F-3", "이름을 입력해주세요");
+		}
+		if (Ut.empty(nickname)) {
+			return ResultData.from("F-4", "닉네임을 입력해주세요");
+		}
+		if (Ut.empty(cellphoneNum)) {
+			return ResultData.from("F-5", "전화번호를 입력해주세요");
+		}
+		if (Ut.empty(email)) {
+			return ResultData.from("F-6", "이메일을 입력해주세요");
+		}
+		ResultData<Integer> joinRd = memberService.join(loginId, loginPw, name, nickname, cellphoneNum, email);
+		if (joinRd.isFail()) {
+			return (ResultData) joinRd;
+		}
+		Member member = memberService.getMemberById(joinRd.getData1());
+		return ResultData.newData(joinRd,"member", member);
+	}
+	
+	
+	
+    // 로그인
 	@RequestMapping("/usr/member/doLogin")
 	@ResponseBody
 	public ResultData<Member> doLogin(HttpSession httpsession, String loginId, String loginPw) {
@@ -52,37 +88,9 @@ public class UsrMemberController {
 
 		return ResultData.from("S-1", Ut.f("%s님 환영합니다", member.getName()));
 	}
+     
 
-	@RequestMapping("/usr/member/doJoin")
-	@ResponseBody
-	public ResultData<Member> doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
-			String email) {
-		if (Ut.empty(loginId)) {
-			return ResultData.from("F-1", "아이디를 입력해주세요");
-		}
-		if (Ut.empty(loginPw)) {
-			return ResultData.from("F-2", "비밀번호를 입력해주세요");
-		}
-		if (Ut.empty(name)) {
-			return ResultData.from("F-3", "이름을 입력해주세요");
-		}
-		if (Ut.empty(nickname)) {
-			return ResultData.from("F-4", "닉네임을 입력해주세요");
-		}
-		if (Ut.empty(cellphoneNum)) {
-			return ResultData.from("F-5", "전화번호를 입력해주세요");
-		}
-		if (Ut.empty(email)) {
-			return ResultData.from("F-6", "이메일을 입력해주세요");
-		}
-		ResultData<Integer> joinRd = memberService.join(loginId, loginPw, name, nickname, cellphoneNum, email);
-		if (joinRd.isFail()) {
-			return (ResultData) joinRd;
-		}
-		Member member = memberService.getMemberById(joinRd.getData1());
-		return ResultData.newData(joinRd,"member", member);
-	}
-
+    // 로그 아웃
 	@RequestMapping("/usr/member/doLoout")
 	@ResponseBody
 	public ResultData<Member> doLogout(HttpSession httpsession) {
